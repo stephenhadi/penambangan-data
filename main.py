122ae2ade5042c5ab61782b35ce83c0d4fb5f27d
stephenhadi <stephenhadi123@gmail.com>
2021-04-07 08:54:49 +0700

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+import nltk
+def main():
+    if len(sys.argv)!= 2:
+        print("masukkan arguments basic atau wa")
+    elif sys.argv[1] =="basic":
+        print("basic")
+
+
+if __name__ == "__main__":
+    main()
