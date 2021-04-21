80fe9e15127da288306e483d81ba759dc185335c
stephenhadi <stephenhadi123@gmail.com>
2021-04-21 14:16:05 +0700

    cosine_simp

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from collections import defaultdict
 from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import cosine_similarity
 import time
 import string
 import pickle
+from nltk.corpus import stopwords 
 
 #Menggunakan Agglomerative   
 #Import library Agglomerative
@@ -31,6 +33,7 @@ def readAll():
             arrText = openFile.read().split("\n")
             myText = ""
             for y in arrText:
+                # cari panjang diatas 9 untuk membuang kalimat kalimat yang tidak penting dan tidak memberikan bobot terlalu besar
                 if len(y)> 9:
                     myText += " "+y
             f[i] = myText
@@ -38,32 +41,25 @@ def readAll():
             print(openFile)
     return f
 
-def read():
-    f = {}
-    openFile=''
-    try:
-        openFile = open("30.txt", 'rb', encoding="utf-8")
-        myText = openFile.read().replace("\n", " ")
-        myText = myText.translate(None,string.punctuation)
-        temp = myText.split('.')
-        newTemp = [word.lstrip() for word in temp if len(word) > 4]
-        f = newTemp
-    except:
-        print(openFile)
-    return f
 
 def vectorize(f):
     fileName = defaultdict(list)
     documents = []
+    sentenceCountPerDoc = defaultdict(int)
+    stop_words = set(stopwords.words('indonesian'))
     for key, value in f.items():
-        tokenText = nltk.sent_tokenize(value)
-
+        words = nltk.sent_tokenize(value.lower())
+        tokenText = [word for word in words if word not in stop_words]
         documents = documents + tokenText
         for z in tokenText:
-            fileName[z].append(key)
-    tfidf = TfidfVectorizer(use_idf=False)
+            # cek apakah nama file yang akan dimasukkan sudah ada atau belum
+            # pasangan fileName {sentences:nama file}
+            if key not in fileName[z]:
+                fileName[z].append(key)
+                sentenceCountPerDoc[key] +=1
+    tfidf = TfidfVectorizer(use_idf=False,stop_words=stop_words)
     result = tfidf.fit_transform(documents)
-    return documents,tfidf,result,fileName
+    return documents,tfidf,result,fileName,sentenceCountPerDoc
 
 
 def main():
@@ -85,7 +81,7 @@ def main():
         array file yang memiliki kalimat tersebut
         
         """ 
-        sentence,tfidf,result,fileName = vectorize(f)
+        sentence,tfidf,result,fileName,sentenceCountPerDoc = vectorize(f)
         df = pd.DataFrame(result.toarray(),columns=tfidf.get_feature_names(),index = sentence)
         #print(df)
 
@@ -99,7 +95,7 @@ def main():
         print(new_df)
 
 
-        # # buat model clustering dengan menggunakan jarak euclidean linkage single
+        # buat model clustering dengan menggunakan jarak euclidean linkage single
         # pkl_filename = "pickle_model.pkl"
         # clustering_model = ""
         # if os.path.isfile(pkl_filename):
@@ -107,7 +103,7 @@ def main():
                 # clustering_model = pickle.load(file)
         
         # else: 
-            # clustering_model = AgglomerativeClustering(distance_threshold=100, n_clusters=None,linkage = 'single', affinity = 'cosine')
+            # clustering_model = AgglomerativeClustering(distance_threshold=1, n_clusters=None,linkage = 'single', affinity = 'cosine')
 
             # clustering_model.fit(new_df)
         
@@ -152,10 +148,64 @@ def main():
                 # doc = fileName[row['Sentence']]
                 # arr = arr + doc
             
-           
+            # arr = list(set(arr))
+            # res = {}
             # print("Cluster ",q," : \n",arr)
 
-        print("program berjalan selama {:.5f} seconds".format(time.time()-start_time))
+        # print("clustering berjalan selama {:.5f} second \n".format(time.time()-start_time))
+        
+        
+        #cosine similarities
+        start_time = time.time()
+        cos_sim = cosine_similarity(new_df)
+        np.fill_diagonal(cos_sim,np.nan)
+        max= np.nanmax(cos_sim,1)
+        max_index = np.nanargmax(cos_sim,1)
+        
+        result = dict()
+        for i in range(len(max)):                        
+            #tempSentences adalah tuple sentences yang mirip yang didapatkan berdasarkan cosine similarities diatas
+            tempSentences = new_df.index[i],new_df.index[max_index[i]]
+                
+            documentPair = "{}|||{}".format(fileName[tempSentences[0]],fileName[tempSentences[1]])
+            documentPairRev = "{}|||{}".format(fileName[tempSentences[1]],fileName[tempSentences[0]])
+            #berfungi menghilangkan noise seperti PT. hlm. tegar jaya. yang dihasilkan oleh tokenize 
+            if len(new_df.index[i]) >15 and fileName[tempSentences[0]] != fileName[tempSentences[1]] :
+                if documentPair in  result:
+                    result[documentPair] +=1
+                else:
+                    if documentPairRev in result:
+                        result[documentPairRev] +=1
+                    else:
+                        result[documentPair] = 1
+            
+        
+        i = 0
+        # perbandingan berapa banyak sentences yang mirip dengan rata rata sentences kedua item
+        print(sentenceCountPerDoc)
+        
+        for key,value in result.items():
+            tempKey = key.replace("',"," '|||")
+            myKey = tempKey.split("|||")
+            average = 0
+            for i in range(len(myKey)):
+                average+= sentenceCountPerDoc.get(myKey[i][2:-2])
+            average = average/len(myKey)
+            result[key] = value/average
+        
+        sorted_result = {k.replace("|||"," "): v for k, v in sorted(result.items(), key= lambda item : item[1], reverse=True)}
+        
+        for key,value in sorted_result.items():
+            
+            print(key,value)
+            i+=1
+            if(i>=10):
+                break
+       
+        
+        print("cosine similarity berjalan selama {:.5f} second".format(time.time()-start_time))
+        
+        
         
         
     elif sys.argv[1] == "wa":  
